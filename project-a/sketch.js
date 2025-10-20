f134dcc875b1638382f28e09b2c95bcfe8c30758
angelifish12 <jc12237@nyu.edu>
2025-10-20 13:53:21 +0800

    new

--- project-a/sketch.js
+++ project-a/sketch.js
@@ -14,6 +14,7 @@ let eyeSizeW2 = 13;
 let eyeSizeW3 = 13;
 let speed = 1;
 let blocking = false;
+let spaceMode = false;
 
 function setup() {
   let canvas = createCanvas(800, 500);
@@ -23,11 +24,14 @@ function setup() {
   circleSize1 = 40;
   circleSize2 = 40;
   circleSize3 = 40;
-  // noLoop();
 }
 
 function draw() {
-  background(20, 50, 95);
+  if (spaceMode == false) {
+    background(random(12, 32), random(45, 65), random(85, 105));
+  }
+
+
   noStroke();
   fill(255);
 
@@ -64,54 +68,25 @@ function draw() {
     }
   }
 
+  // stars
   let starCount = 200;
   for (let i = 0; i < starCount; i++) {
-    let x = random(width);
-    let y = random(height);
-    let d = random(1, 3); //tiny dots
-    circle(x, y, d);
+    let sx = random(width);
+    let sy = random(height);
+    let sd = random(1, 3); // tiny dots
+    circle(sx, sy, sd);
   }
 
   if (mouseIsPressed) {
-    let lightSize = 180;
-    push();
-    fill(255, 239, 145);
-    circle(mouseX, mouseY, lightSize);
-    pop();
-    // let lightDist1 = dist(mouseX, mouseY, width / 2, height / 2);
-    // let lightDist2 = dist(mouseX, mouseY, width / 2 + 100, height / 2 + 150);
-    // let lightDist3 = dist(mouseX, mouseY, width / 2 - 150, height / 2 + 100);
-    // if (lightDist1 < 30) {
-    //   circleSize1 = 0;
-    //   eyeSize1 = 0;
-    //   eyeSizeW1 = 0;
-    //   speed = 0;
-    // }
+    // flashlight
+    fill(255, 220, 100, 220);
+    ellipse(mouseX, mouseY, 50, 50);
+    fill(255, 245, 170, 100);
+    triangle(mouseX, mouseY - 25, mouseX, mouseY + 25, mouseX - 360, mouseY);
   } else {
-    drawCreature(
-      width / 2,
-      height / 2,
-      circleSize1,
-      lineWidth1,
-      eyeSize1,
-      eyeSizeW1
-    );
-    drawCreature(
-      width / 2 + 100,
-      height / 2 + 150,
-      circleSize2,
-      lineWidth2,
-      eyeSize2,
-      eyeSizeW2
-    );
-    drawCreature(
-      width / 2 - 150,
-      height / 2 + 100,
-      circleSize3,
-      lineWidth3,
-      eyeSize3,
-      eyeSizeW3
-    );
+    drawCreature(width / 2, height / 2, circleSize1, lineWidth1, eyeSize1, eyeSizeW1);
+    drawCreature(width / 2 + 100, height / 2 + 150, circleSize2, lineWidth2, eyeSize2, eyeSizeW2);
+    drawCreature(width / 2 - 150, height / 2 + 100, circleSize3, lineWidth3, eyeSize3, eyeSizeW3);
   }
 }
 
@@ -119,27 +94,22 @@ function drawCreature(cx, cy, size, lineWidth, eyeSize, eyeSizeW) {
   push();
 
   translate(cx, cy);
-  //move
   let v = 2 * sin(frameCount * 0.1);
-  // Sprite A
-  noStroke();
 
+  noStroke();
   let d = dist(mouseX, mouseY, cx, cy);
   if (d < 100 / 2) {
     fill(0, 128, 128);
-    //circleSize = circleSize + 1;
   } else {
     fill(30);
     stroke(20);
   }
-  //moving like bouncy
+
   translate(0, 20 * sin(frameCount * 0.1));
-  // x axis move side to side, y axis move up and down;
 
   push();
   fill(30);
   stroke(20);
-  // spikes
   line(0 + 14 - v, 0 + v, 0 + 32 - v + lineWidth, 0 + v);
   line(0 + 12 - v, 0 + 7 + v, 0 + 28 - v + lineWidth, 0 + 16 + v + lineWidth);
   line(0 + 10 - v, 0 + 10 + v, 0 + 23 - v + lineWidth, 0 + 23 + v + lineWidth);
@@ -158,22 +128,31 @@ function drawCreature(cx, cy, size, lineWidth, eyeSize, eyeSizeW) {
   line(0 + 12 - v, 0 - 7 + v, 0 + 28 - v + lineWidth, 0 - 16 + v - lineWidth);
   pop();
 
-  // body
   circle(0 - v, 0 + v, size);
 
-  // eyes (A)
   noStroke();
-
-  fill(255); // whites
+  fill(255);
   circle(0 - 7 + v, 0 - 5 + v, eyeSizeW);
   circle(0 + 9 + v, 0 - 5 + v, eyeSizeW);
-  fill(20); // pupils
+  fill(20);
   circle(x - 7 + v, y + 0 - 4 + v, eyeSize);
   circle(x + 6 + v, y + 0 - 4 + v, eyeSize);
-  // circle(x, height/2, 50);
+
   x = mouseX * 0.01 - 2;
   y = mouseY * 0.01 - 2;
   pop();
 
   return d;
+}
+//change background to a cool version
+function keyPressed() {
+  if (key == ' ') {
+    spaceMode = true;
+    background(22, 55, 95);
+  }
+}
+function keyReleased() {
+  if (key == ' ') {
+    spaceMode = false;
+  }
 }
